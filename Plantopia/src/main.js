e1982ac6536d9d1e9518a635007cf89d4c67c0a6
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-10-21 14:26:30 +0200

    Add route path

--- Plantopia/src/main.js
+++ Plantopia/src/main.js
@@ -1,6 +1,33 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
+import Leaderboard from './components/views/Leaderboard.vue'
+import Shop from './components/views/Shop.vue'
+import Inventory from './components/views/Inventory.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/leaderboard',
+            name: 'Leaderboard',
+            component: Leaderboard
+        },
+        {
+            path: '/shop',
+            name: 'Shop',
+            component: Shop
+        },
+        {
+            path: '/inventory',
+            name: 'Inventory',
+            component: Inventory
+        },
+    ]
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
